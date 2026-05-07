4cda53843d5cbe3df332064a617121013160975e
Kisaragi Hiu <mail@kisaragi-hiu.com>
2026-05-08 00:41:06 +0900

    fix neutral tones not included

--- processing/lib/pnToInputForm.ts
+++ processing/lib/pnToInputForm.ts
@@ -11,10 +11,10 @@ export function pnToInputForm(pn: string) {
     "\u030d": "8",
     "\u0306": "9",
   } as const;
-  type Tones = typeof tones;
-  type ToneMark = keyof Tones;
   let chars: string[] = [];
-  let currentTone: Tones[ToneMark] | undefined = undefined;
+  let previousChar: string | undefined = undefined;
+  let currentTone: // prettier-ignore
+  "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | undefined = undefined;
   for (const char of pn
     .replaceAll("ⁿ", "nn")
     .replaceAll("o͘", "oo")
@@ -28,26 +28,43 @@ export function pnToInputForm(pn: string) {
     .normalize("NFKD")) {
     if (char in tones) {
       // We have seen a tone character. Log it and continue
-      currentTone = tones[char as ToneMark];
-      continue;
-    }
-    if (!si_lomaji(char) && currentTone) {
-      // char is not lomaji and there is a pending tone. We have just ended a
-      // syllable. Push through the tone, reset it, then push through this character.
-      chars.push(currentTone);
-      currentTone = undefined;
+      // (There is not supposed to be multiple in a syllable)
+      currentTone = tones[char as keyof typeof tones];
+    } else if (!si_lomaji(char)) {
+      // char is not lomaji. We have just ended a syllable.
+      // Push the tone out, reset it, then push through this character.
+      // If there is no tone: the tone is neutral (4 or 1).
+      // If there is no tone and also no previous character: there's an invalid
+      // syllable. Just, uh, push the character out and move on.
+      if (
+        !currentTone &&
+        previousChar &&
+        // if the previous character is not lomaji, it's not valid, but it's
+        // useful to not add a 1 to the end of it, so avoid doing so.
+        si_lomaji(previousChar)
+      ) {
+        currentTone = /[ptkh]/.test(previousChar) ? "4" : "1";
+      }
+      if (currentTone) {
+        chars.push(currentTone);
+        currentTone = undefined;
+      }
       chars.push(char);
-      continue;
+    } else {
+      // Default case: push it through
+      chars.push(char);
     }
 
-    // Default case: push it through
-    chars.push(char);
+    previousChar = char;
   }
   // There might still be a pending tone after the last character. Check it, and
   // if so, push it through.
   if (currentTone) {
     chars.push(currentTone);
     currentTone = undefined;
+  } else if (previousChar) {
+    // or we might have ended with a neutral tone.
+    chars.push(/[ptkh]/.test(previousChar) ? "4" : "1");
   }
   return chars.join("");
 }
