ddc8dae51c541d4a4a3e4719e99a3696bfac97b4
Kisaragi Hiu <mail@kisaragi-hiu.com>
2026-05-29 20:24:20 +0900

    refactor out word interation to its own file
    This prepares it for word frequencies

---
+++ processing/lib/words.ts
@@ -0,0 +1,123 @@
+import { z } from "zod";
+import { Database } from "bun:sqlite";
+import { existsSync } from "node:fs";
+
+function cleanTitle(title: string) {
+  return title
+    .replaceAll(/\*\*/gv, "")
+    .replaceAll(/\(\*[浦泉漳同白文安]?\)/gv, "")
+    .replaceAll(/\(漳[，,]XX\)/gv, "")
+    .replaceAll(/^(:|##|【俗】)/gv, "");
+}
+
+function cleanPn(pn: string) {
+  return cleanTitle(pn);
+}
+
+function getWords(): Array<{ title: string; pn: string }> {
+  const kemdictDir = "../../kemdict/";
+  const kemdictDb = kemdictDir + "dicts/entries.db";
+  if (!existsSync(kemdictDb)) {
+    console.log(`${kemdictDb} not found`);
+    process.exit(1);
+  }
+  console.log("Connecting to local Kemdict database...");
+  const db = new Database(kemdictDb, {
+    readonly: true,
+    create: false,
+    strict: true,
+  });
+  const wordsSchema = z.array(
+    z.object({
+      title: z.string(),
+      pn: z.string(),
+    }),
+  );
+  console.log("Retrieving words...");
+  const rawWords = wordsSchema.parse(
+    db
+      .prepare(
+        `
+SELECT DISTINCT title, aliases.alias as pn FROM heteronyms
+LEFT JOIN aliases ON heteronyms.id = aliases.het_id
+WHERE lang = 'nan_TW'
+  AND pn != title
+  AND aliases.exact = 1
+  AND "from" != 'chhoetaigi_itaigi'
+ORDER BY title
+`,
+      )
+      .all(),
+  );
+  const words: typeof rawWords = [];
+
+  for (const { title, pn } of rawWords) {
+    if (title.trim() === "") continue;
+    if (pn.trim() === "") continue;
+    if (
+      title.startsWith("(") ||
+      /。|\.|,/.test(title) ||
+      title.startsWith("...") ||
+      pn.startsWith("*") ||
+      pn.startsWith("[") ||
+      pn.startsWith('"') ||
+      /\.|,|\?/.test(pn) ||
+      /^[0-9]/v.test(pn) ||
+      /^\p{Script=Han}/v.test(pn)
+    ) {
+      continue;
+    }
+    // Special case: if the title has full width parens and the pn has half
+    // width parens, the contents of the parens correspond with each other.
+    // For example, 富貴（南河） -> Hù-kuì(Lâm-hô)
+    // This is different from the paren mess with Maryknoll
+    if (/（.*）/.test(title) && /\(.*\)/.test(pn)) {
+      words.push({
+        title: cleanTitle(title.replaceAll(/（.*）/g, "")),
+        pn: cleanPn(pn.replaceAll(/\(.*\)/g, "")),
+      });
+      words.push({
+        title: cleanTitle(title.match(/（(.*)）/)![1]),
+        pn: cleanPn(pn.match(/\((.*)\)/)![1]),
+      });
+      continue;
+    }
+
+    const cleanedTitle = cleanTitle(title);
+    const cleanedPn = cleanPn(pn);
+
+    // Just get rid of all the other parens. This is better than giving up Maryknoll...
+    if (/\(|\)|（|）/.test(cleanedTitle) || /\(|\)|（|）/.test(cleanedPn)) {
+      continue;
+    }
+
+    words.push({
+      title: cleanedTitle,
+      pn: cleanedPn,
+    });
+  }
+
+  return words;
+}
+
+export async function forEachWord(
+  before: (length: number) => void,
+  body: (word: { title: string; pn: string }) => Promise<void>,
+) {
+  const rawWords = getWords();
+  before(rawWords.length);
+  const newlines: [string, string][] = [];
+  for (const { title, pn } of rawWords) {
+    if (title.includes("\n") || pn.includes("\n")) {
+      newlines.push([title, pn]);
+    }
+  }
+  if (newlines.length > 0) {
+    console.log("Some entries have newlines! This should not be the case");
+    console.log("Entries with newlines:", newlines);
+    process.exit(1);
+  }
+  for (const rawWord of rawWords) {
+    await body(rawWord);
+  }
+}
--- processing/writeWords.ts
+++ processing/writeWords.ts
@@ -4,112 +4,12 @@ import {
   toPOJBulk as toPOJBulkNative,
 } from "./lib/pojtl-native.ts";
 
-import { parseArgs } from "node:util";
-
+import { forEachWord } from "./lib/words.ts";
 import { pnToInputForm } from "./lib/pnToInputForm.ts";
 
-import { z } from "zod";
-import { Database } from "bun:sqlite";
-import { writeFileSync, existsSync } from "node:fs";
+import { parseArgs } from "node:util";
+import { writeFileSync } from "node:fs";
 import { resolve } from "node:path";
-
-function cleanTitle(title: string) {
-  return title
-    .replaceAll(/\*\*/gv, "")
-    .replaceAll(/\(\*[浦泉漳同白文安]?\)/gv, "")
-    .replaceAll(/\(漳[，,]XX\)/gv, "")
-    .replaceAll(/^(:|##|【俗】)/gv, "");
-}
-
-function cleanPn(pn: string) {
-  return cleanTitle(pn);
-}
-
-function getWords(): Array<{ title: string; pn: string }> {
-  const kemdictDir = "../../kemdict/";
-  const kemdictDb = kemdictDir + "dicts/entries.db";
-  if (!existsSync(kemdictDb)) {
-    console.log(`${kemdictDb} not found`);
-    process.exit(1);
-  }
-  console.log("Connecting to local Kemdict database...");
-  const db = new Database(kemdictDb, {
-    readonly: true,
-    create: false,
-    strict: true,
-  });
-  const wordsSchema = z.array(
-    z.object({
-      title: z.string(),
-      pn: z.string(),
-    }),
-  );
-  console.log("Retrieving words...");
-  const rawWords = wordsSchema.parse(
-    db
-      .prepare(
-        `
-SELECT DISTINCT title, aliases.alias as pn FROM heteronyms
-LEFT JOIN aliases ON heteronyms.id = aliases.het_id
-WHERE lang = 'nan_TW'
-  AND pn != title
-  AND aliases.exact = 1
-  AND "from" != 'chhoetaigi_itaigi'
-ORDER BY title
-`,
-      )
-      .all(),
-  );
-  const words: typeof rawWords = [];
-
-  for (const { title, pn } of rawWords) {
-    if (title.trim() === "") continue;
-    if (pn.trim() === "") continue;
-    if (
-      title.startsWith("(") ||
-      /。|\.|,/.test(title) ||
-      title.startsWith("...") ||
-      pn.startsWith("*") ||
-      pn.startsWith("[") ||
-      pn.startsWith('"') ||
-      /\.|,|\?/.test(pn) ||
-      /^[0-9]/v.test(pn) ||
-      /^\p{Script=Han}/v.test(pn)
-    ) {
-      continue;
-    }
-    // Special case: if the title has full width parens and the pn has half
-    // width parens, the contents of the parens correspond with each other.
-    // For example, 富貴（南河） -> Hù-kuì(Lâm-hô)
-    // This is different from the paren mess with Maryknoll
-    if (/（.*）/.test(title) && /\(.*\)/.test(pn)) {
-      words.push({
-        title: cleanTitle(title.replaceAll(/（.*）/g, "")),
-        pn: cleanPn(pn.replaceAll(/\(.*\)/g, "")),
-      });
-      words.push({
-        title: cleanTitle(title.match(/（(.*)）/)![1]),
-        pn: cleanPn(pn.match(/\((.*)\)/)![1]),
-      });
-      continue;
-    }
-
-    const cleanedTitle = cleanTitle(title);
-    const cleanedPn = cleanPn(pn);
-
-    // Just get rid of all the other parens. This is better than giving up Maryknoll...
-    if (/\(|\)|（|）/.test(cleanedTitle) || /\(|\)|（|）/.test(cleanedPn)) {
-      continue;
-    }
-
-    words.push({
-      title: cleanedTitle,
-      pn: cleanedPn,
-    });
-  }
-
-  return words;
-}
 
 async function writeDict(
   path: string,
@@ -117,48 +17,40 @@ async function writeDict(
   type: "kip" | "poj",
   ipc?: boolean,
 ) {
-  const rawWords = getWords();
+  let i = 0;
   let lines: string[] = [];
   let essayLines = new Set<string>();
-  let i = 0;
   const titles = new Set<string>();
   const pns = new Set<string>();
-  console.log(`Converting raw words (total ${rawWords.length})...`);
-  const newlines: [string, string][] = [];
-  for (const { title, pn } of rawWords) {
-    if (title.includes("\n") || pn.includes("\n")) {
-      newlines.push([title, pn]);
-    }
-  }
-  if (newlines.length > 0) {
-    console.log("Some entries have newlines! This should not be the case");
-    console.log("Entries with newlines:", newlines);
-    process.exit(1);
-  }
-  for (const { title, pn } of rawWords) {
-    if (title.startsWith("-")) continue;
-    let [nPn, nTitle] =
-      type === "kip"
-        ? await (ipc ? toKIPBulk : toKIPBulkNative)([pn, title])
-        : await (ipc ? toPOJBulk : toPOJBulkNative)([pn, title]);
-    const inputForm = pnToInputForm(nPn);
-    if (!(titles.has(nTitle) && pns.has(nPn))) {
-      titles.add(nTitle);
-      pns.add(nPn);
-      lines.push(`${nTitle}\t${inputForm}`);
-      essayLines.add(nTitle);
-    }
-    if (!(titles.has(nPn) && pns.has(nPn))) {
-      titles.add(nPn);
-      pns.add(nPn);
-      lines.push(`${nPn}\t${inputForm}`);
-      essayLines.add(nPn);
-    }
-    i++;
-    if (i % 1000 === 0) {
-      console.log(`${new Date().toISOString()}: convert ${i}`);
-    }
-  }
+  await forEachWord(
+    (length) => {
+      console.log(`Converting raw words (total ${length})...`);
+    },
+    async ({ pn, title }) => {
+      if (title.startsWith("-")) return;
+      let [nPn, nTitle] =
+        type === "kip"
+          ? await (ipc ? toKIPBulk : toKIPBulkNative)([pn, title])
+          : await (ipc ? toPOJBulk : toPOJBulkNative)([pn, title]);
+      const inputForm = pnToInputForm(nPn);
+      if (!(titles.has(nTitle) && pns.has(nPn))) {
+        titles.add(nTitle);
+        pns.add(nPn);
+        lines.push(`${nTitle}\t${inputForm}`);
+        essayLines.add(nTitle);
+      }
+      if (!(titles.has(nPn) && pns.has(nPn))) {
+        titles.add(nPn);
+        pns.add(nPn);
+        lines.push(`${nPn}\t${inputForm}`);
+        essayLines.add(nPn);
+      }
+      i++;
+      if (i % 1000 === 0) {
+        console.log(`${new Date().toISOString()}: convert ${i}`);
+      }
+    },
+  );
 
   console.log(`Writing ${type} words dict to ${resolve(path)}...`);
   writeFileSync(
